69ab6ee2a507f7b27325571c3476927734909605
Waylon Li <44042246+waylonli@users.noreply.github.com>
2023-03-02 19:20:01 +0000

    Update convert_to_csv.sh

--- preprocess/convert_to_csv.sh
+++ preprocess/convert_to_csv.sh
@@ -1,6 +1,6 @@
 SYM=conservation
 DATASETPATH=../datasets/unmixed
 
-python ./convert_to_csv.py ${DATASETPATH}/${SYM}_train ${DATASETPATH}/${SYM}_train.csv
-python ./convert_to_csv.py ${DATASETPATH}/${SYM}_dev ${DATASETPATH}/${SYM}_dev.csv
-python ./convert_to_csv.py ${DATASETPATH}/${SYM}_test ${DATASETPATH}/${SYM}_test.csv
+python ../train/convert_to_csv.py ${DATASETPATH}/${SYM}_train ${DATASETPATH}/${SYM}_train.csv
+python ../train/convert_to_csv.py ${DATASETPATH}/${SYM}_dev ${DATASETPATH}/${SYM}_dev.csv
+python ../train/convert_to_csv.py ${DATASETPATH}/${SYM}_test ${DATASETPATH}/${SYM}_test.csv
