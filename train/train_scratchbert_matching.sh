3c093d8d1c65c0f371e6c10f02f97e9b7c096c06
Waylon Li <44042246+waylonli@users.noreply.github.com>
2023-03-02 19:12:16 +0000

    Update train_scratchbert_matching.sh

--- train/train_scratchbert_matching.sh
+++ train/train_scratchbert_matching.sh
@@ -2,13 +2,13 @@ ANNO=partial
 INPUT=math
 
 MODELPATH=./${ANNO}_${INPUT}_local
-PRETRAINPATH=../pretrain/model_files
+PRETRAINPATH=../model_files
 
 # train local model
 python neural_model_bert.py train $MODELPATH \
-./datasets/${ANNO}_anno_train.csv \
-./datasets/${ANNO}_anno_dev.csv \
-./datasets/full_anno_dev.csv \
+../datasets/${ANNO}_anno_train.csv \
+../datasets/${ANNO}_anno_dev.csv \
+../datasets/full_anno_dev.csv \
 --seed 10000 -v 40 -i 60 -N 16 -L softmax -W 300 -d 2 --dk 128 --n-heads 4 --max-length 200 \
 --input ${INPUT} --optimizer asgd -l 2e-3 --gpu 0 \
 --pretrainpath ${PRETRAINPATH}
@@ -24,6 +24,6 @@ python neural_model_bert.py train $MODELPATH \
 
 # eval
 python neural_model_bert.py eval $MODELPATH \
-./datasets/conservation_test.csv \
+../datasets/conservation_test.csv \
 . --gpu 0 --max_length 200 --input ${INPUT} \
 --pretrainpath ${PRETRAINPATH}
